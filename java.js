b5190347126cf68ca28346d1f9d061b6e5d80053
Melle Siegers <34574@ma-web.nl>
2023-03-08 15:29:57 +0100

    reset button

--- java.js
+++ java.js
@@ -1,31 +1,42 @@
 const startButton = document.getElementById("js--start");
 const stopButton = document.getElementById("js--stop");
+const resetButton = document.getElementById("js--reset");
 let seconds = 0;
 let minutes = 0;
 let running = false;
 
-const secondsTimer = document.getElementById("js--secondstimer");
+const secondstimer = document.getElementById("js--secondstimer");
 const minutestimer = document.getElementById("js--minutestimer");
 
 let timer;
 
-startButton.onclick = function(){
-    if(running === true){
+startButton.onclick = function () {
+    if (running === true) {
         return;
     }
     running = true;
-    timer = setInterval(function(){
+    timer = setInterval(function () {
         seconds = seconds + 1;
-        if(seconds === 60){
+        if (seconds === 60) {
             minutes = minutes = 1;
             minutestimer.innerText = minutes;
             seconds = 0;
         }
-        secondsTimer.innerText = seconds;
-    }, 1000);  
+        secondstimer.innerText = seconds;
+    }, 1000);
 }
 
-stopButton.onclick = function(){
+stopButton.onclick = function () {
     clearInterval(timer);
     running = false;
 }
+
+resetButton.onclick = function () {
+    clearInterval(timer)
+    seconds = 0;
+    minutes = 0;
+    secondstimer.innerText = 0;
+    minutestimer.innerText = 0;
+    running = false;
+}
+
